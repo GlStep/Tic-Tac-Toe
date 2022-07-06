fcf8ee537a4f6109da122456aba93f3aac482a3d
Gleb Stepanov <gleb.stepanov@online.de>
2022-07-06 13:50:40 +0200

    create main.cpp

---
+++ Tic-Tac-Toe_V2/main.cpp
@@ -0,0 +1,12 @@
+#include "tic_fnc.hpp"
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+
+    //Creating instances of the two players, which will play againt each other
+    Player player1;
+    Player player2;
+}
