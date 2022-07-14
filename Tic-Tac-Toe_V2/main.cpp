9648eca2b44e83e3902c5d5e5fdebb4af154f427
Gleb Stepanov <gleb.stepanov@online.de>
2022-07-14 11:09:41 +0200

    added setter and getter

--- Tic-Tac-Toe_V2/main.cpp
+++ Tic-Tac-Toe_V2/main.cpp
@@ -12,4 +12,10 @@ int main() {
 
     //Creating instance of the field, which will include all the field logic
     Field playing_field;
+
+    //Starting by asking the name of the player
+    player1.set_name(1);
+    player2.set_name(2);
+
+    std::cout << player1.get_name() << std::endl;
 }
