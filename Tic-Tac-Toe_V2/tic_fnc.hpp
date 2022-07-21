61448f409db0a9b8f9c70faada07cf5e5aed7e12
Gleb Stepanov <59051931+GlStep@users.noreply.github.com>
2022-07-21 22:18:56 +0200

    Tic tac toe v2 (#3)
    * Added more functionality.
    
    * modified cmake-build-debug
    
    * Updated cmake-build-debug
    
    * Added all the desired bare functionality
    
    * Finished bare game main.cpp
    
    * Updates

--- Tic-Tac-Toe_V2/tic_fnc.hpp
+++ Tic-Tac-Toe_V2/tic_fnc.hpp
@@ -6,28 +6,35 @@
 #include <iostream>
 
 class Player {
-private: 
-    int player_number_;
+private:
+    int row_{}, column_{};
+    int player_number_{};
     std::string player_name_;
 
 public:
     Player();
-    std::string set_name(int player_number);
+    void set_name(int player_number);
     std::string get_name();
-    int data_player_row();
-    int data_player_column();
+    void set_data_player_row();
+    int get_data_player_row();
+    void set_data_player_column();
+    int get_data_player_column();
 };
 
 class Field {
 private: 
     int row_;
     int column_;
+    int player_number_;
+    std::string playfield_[3][3];
 
 public:
     Field();
+    void create_playing_field();
     void start_game();
     void show_field();
-    int check_space();
+    bool check_space(int row, int column);
+    void write_field(int row, int column, int player_number);
     bool check_win(int row, int column);
 };
 
